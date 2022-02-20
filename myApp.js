231f5b61638b4cc651c4d53e7e8aef3f6881eba9
Mars2point0 <stardustermars@gmail.com>
2022-02-20 21:56:54 +0200

    second commit

---
+++ myApp.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var app = express();
+
+console.log('Hello World');
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", function(req, res){
+  res.json({
+    "message": "Hello json"
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
